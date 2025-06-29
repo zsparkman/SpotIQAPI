0ac525a9633fa2b4ec31d94b7a97a3563cd857e0
zsparkman <zsparkman907@gmail.com>
2025-06-28 19:31:59 -0700

    Delete parsers/parser_tmpiduqvsjn_csv_1751161828.py

--- parsers/parser_tmpiduqvsjn_csv_1751161828.py
+++
@@ -1,9 +0,0 @@
-def parse(file_path):
-    with open(file_path, 'r') as f:
-        lines = f.readlines()
-    keys = lines[0].strip().split(',')
-    data = []
-    for line in lines[1:]:
-        values = line.strip().split(',')
-        data.append(dict(zip(keys, values)))
-    return data
